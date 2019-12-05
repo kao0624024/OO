675bd76ecbd8c925b0b6aa2d601fcdaa2436e1ac
kao0624024 <54345822+kao0624024@users.noreply.github.com>
2019-12-05 15:09:26 +0800

    Add files via upload

---
+++ 1205/student.java
@@ -0,0 +1,13 @@
+class Student{
+  Course course[] = new Course[10];
+}
+
+class Course{
+}
+
+class main{
+  public static void main(String args[]){
+    Student student = new Student();
+    System.out.println("ok");
+  }
+}
